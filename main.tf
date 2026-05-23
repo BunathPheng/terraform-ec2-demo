e6fa3652f462d8ca8b7f9ed2db0b24ddb58837e1
Pheng Bunath <bunathpheng4@gmail.com>
2026-05-23 10:51:57 +0700

    Add Terraform configuration for AWS resources
    This Terraform configuration sets up an AWS provider, retrieves the latest Ubuntu AMI, creates a security group allowing SSH and HTTP access, and launches an EC2 instance with the specified AMI and security group.

---
+++ main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+# Get latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+# Security Group
+resource "aws_security_group" "demo_sg" {
+  name        = "terraform-demo-sg-3"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "demo" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+
+  tags = {
+    Name = "terraform-ubuntu"
+  }
+}
